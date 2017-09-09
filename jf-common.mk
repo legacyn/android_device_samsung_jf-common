f24f9e381b8a853800db2d3fda8985fb64a407b1
Marco Zanin (B--B) <mrczn.bb@gmail.com>
2017-09-09 02:50:26 +0200

    TEMP: do not build NFC HAL
    Until bt is fixed it just spams logs
    
    Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>

--- jf-common.mk
+++ jf-common.mk
@@ -159,8 +159,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
 
 # NFC packages
-PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.0-impl-bcm \
+#PRODUCT_PACKAGES += \
     libnfc-nci \
     libnfc_nci_jni \
     nfc_nci.bcm2079x.default \
