aaa4e27fd857575408872e0412bbb0862426121d
Marco Zanin (B--B) <mrczn.bb@gmail.com>
2017-09-28 17:48:23 +0200

    NFC: move NFC configs to vendor
    Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>

--- jf-common.mk
+++ jf-common.mk
@@ -180,8 +180,8 @@ PRODUCT_PACKAGES += \
     com.android.nfc_extras
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
-    $(LOCAL_PATH)/configs/nfcee_access.xml:system/etc/nfcee_access.xml
+    $(LOCAL_PATH)/configs/libnfc-brcm.conf:system/vendor/etc/libnfc-brcm.conf \
+    $(LOCAL_PATH)/configs/nfcee_access.xml:system/vendor/etc/nfcee_access.xml
 
 # OMX
 PRODUCT_PACKAGES += \
