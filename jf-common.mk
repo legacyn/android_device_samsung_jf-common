136c0139be8b14b856ad0cc48ae2baf25b9bdd72
Buff99 <buffbuffster@gmail.com>
2018-05-21 20:33:07 +0100

    jf-common: finish adding spectrum this time

--- jf-common.mk
+++ jf-common.mk
@@ -197,6 +197,8 @@ PRODUCT_PACKAGES += \
     init.qcom.power.rc \
     init.qcom.usb.rc \
     init.recovery.qcom.rc \
+    init.spectrum.rc \
+    init.spectrum.sh \
     ueventd.qcom.rc
 
 # Seccomp policy
