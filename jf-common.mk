eca610af2b4fd5966214f9e1280757555e42b839
Buff99 <buffbuffster@gmail.com>
2018-05-28 00:44:17 +0100

    js-common: add weather providers

--- jf-common.mk
+++ jf-common.mk
@@ -223,6 +223,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libcnefeatureconfig
 
+# Weather providers
+PRODUCT_PACKAGES += \
+	OpenWeatherMapWeatherProvider \
+	YahooWeatherProvider \
+	WundergroundWeatherProvider
+
 # Wifi
 PRODUCT_PACKAGES += \
     libnetcmdiface \
