523d3b864ee2c403db5e79c2c29ff249b1e91dac
Vlad <tsvetkov_vlad@ukr.net>
2018-05-28 22:07:43 +0300

    jf: remove unused packages

--- jf-common.mk
+++ jf-common.mk
@@ -107,10 +107,7 @@ PRODUCT_PACKAGES += \
     copybit.msm8960 \
     gralloc.msm8960 \
     hwcomposer.msm8960 \
-    libgenlock \
-    memtrack.msm8960 \
-    libqdutils \
-    libqdMetaData
+    memtrack.msm8960
 
 # Doze
 PRODUCT_PACKAGES += \
